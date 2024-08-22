f4eb3992baa841d35f1fac851c3039ef45f70916
devkgn88 <devkgn88@gmail.com>
2024-08-22 15:27:50 +0900

    [김가남] 최초 commit

---
+++ src/com/gn/day01/Run.java
@@ -0,0 +1,13 @@
+package com.gn.day01;
+
+public class Run {
+	public static void main(String[] args) {
+		Solution01 s1 = new Solution01();
+		
+		s1.solution01(2, 3);
+		s1.solution01(100,2);
+		
+		s1.solution02(2, 3);
+		s1.solution02(100,2);
+	}
+}
---
+++ src/com/gn/day01/Solution01.java
@@ -0,0 +1,15 @@
+package com.gn.day01;
+
+public class Solution01 {
+	
+	public int solution01(int num1, int num2) {
+		int answer = -1;
+		answer = num1 + num2;
+		return answer;
+	}
+
+	
+	public int solution02(int num1, int num2) {
+        return num1 + num2;
+    }
+}
