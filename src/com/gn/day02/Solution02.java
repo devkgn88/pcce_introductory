ce8ef62d07bacfa7605046c886eaabe246bf89d3
devkgn88 <devkgn88@gmail.com>
2024-08-25 21:58:09 +0900

    [Kimganam] Commit test

--- src/com/gn/day02/Solution02.java
+++ src/com/gn/day02/Solution02.java
@@ -7,7 +7,7 @@ public class Solution02 {
 	public int solution01(int num1, int num2) {
 		int answer = -1;
 		if(num1 == num2) answer = 1;
-		return answer;
+		return answer; 
 	}
 	
 	public int solution02(int num1, int num2) {
