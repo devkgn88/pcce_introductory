d235701ce8ba67535cab3cdd63836f37e610ef46
devkgn88 <devkgn88@gmail.com>
2024-08-28 09:47:11 +0900

    [KimGanam] Cut String and Save as Array

--- src/com/gn/day19/Run.java
+++ src/com/gn/day19/Run.java
@@ -4,8 +4,18 @@ public class Run {
 
 	public static void main(String[] args) {
 		Solution02 s2 = new Solution02();
-		s2.solution01("abc1Addfggg4556b", 6);
-		s2.solution01("abcdef123", 3);
+		System.out.println("===== 1번 =====");
+//		String[] arr1 = s2.solution01("abc1Addfggg4556b", 6);
+//		for(String s : arr1) {
+//			System.out.println(s);
+//		}
+		s2.solution03("abc1Addfggg4556b", 6);
+		System.out.println("===== 2번 =====");
+//		String[] arr2 = s2.solution01("abcdef123", 3);
+//		for(String s : arr2) {
+//			System.out.println(s);
+//		}
+		s2.solution03("abcdef123", 3);
 		
 	}
 
--- src/com/gn/day19/Solution02.java
+++ src/com/gn/day19/Solution02.java
@@ -1,30 +1,71 @@
 package com.gn.day19;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution02 {
 	// When the strings my_str and n are given as parameters
 	// complete the solution function
 	// to return an array that cuts my_str
 	// by length n and stores it.
+	
+	public String[] solution03(String my_str, int n) {
+		int leng = my_str.length();
+		int num = (leng / n) + (leng %n == 0 ? 0 : 1);
+		String[] result = new String[num];
+		for(int i = 0 ; i < num ; i++) {
+			int start = i*n;
+			int end = i*n+n > leng ? leng : i*n +n;
+			result[i] = my_str.substring(start,end);
+		}
+		
+		return result;
+	}
+	
     public String[] solution01(String my_str, int n) {
         String[] arr = my_str.split("");
         
         int leng = arr.length;
-        int result = (leng / n) + (leng %n != 0 ? 0 : 1);
+        int num = (leng / n) + (leng %n == 0 ? 0 : 1);
         
-        String[] answer = new String[leng];
+        String[] result = new String[num];
         
-        int idx = 0;
-        for(int i = 0 ; i < result ; i++) {
-        	String temp = "";
-        	for(int j = 0 ; j < n ; j++) {
-        		temp += arr[j+(idx*n)];
-        		System.out.println(j+(idx*n));
+        int cnt = 1 ;
+        int idx = 0 ;
+        String word = "";
+        
+        for(int i = 0 ; i < leng ; i++) {
+        	word += arr[i];
+        	if(cnt == n || i == leng-1) {
+        		result[idx++] = word;
+        		cnt = 1;
+        		word = "";
+        	} else {
+        		cnt ++;
         	}
-        	System.out.println(temp);
-        	answer[idx] = temp;
-        	idx++;
         }
-        
-        return answer;
+        return result;
     }
+    
+    public List<String> solution02(String my_str, int n) {
+    	List<String> arr = new ArrayList<String>(Arrays.asList(my_str.split("")));
+    	List<String> result = new ArrayList<String>();
+    	String str = "";
+    	int cnt = 1;
+    	while(arr.size() > 0) {
+    		str += arr.get(0);
+    		arr.remove(0);
+    		if(cnt == n || arr.size() == 0) {
+    			result.add(str);
+    			str = "";
+    			cnt = 1; 
+    		}else {
+    			cnt++;
+    		}
+    	}
+    	return result;
+    }
+    
+    
 }
