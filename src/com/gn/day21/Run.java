761aa378803eaa97a4b293da594fd951f9500356
devkgn88 <devkgn88@gmail.com>
2024-08-29 12:17:21 +0900

    [KimGanam] find Num between String and add

--- src/com/gn/day21/Run.java
+++ src/com/gn/day21/Run.java
@@ -1,5 +1,9 @@
 package com.gn.day21;
 
 public class Run {
-
+	public static void main(String[] args) {
+		Solution01 s1 = new Solution01();
+		s1.solution02("aAb1B2cC34oOp"); // 1 + 2 + 34
+		s1.solution02("1a2b3c4d123Z"); // 1 + 2 + 3 + 4 + 123
+	}
 }
--- src/com/gn/day21/Solution01.java
+++ src/com/gn/day21/Solution01.java
@@ -6,4 +6,30 @@ public class Solution01 {
 	// uppercase letters, and natural numbers
 	// complete the solution function to
 	// return the sum of the natural numbers in my_string
+    public int solution01(String my_string) {
+        int answer = 0;
+        String[] arr = my_string.split("");
+        String num = "";
+        for(String str : arr) {
+        	if(str.matches("[+-]?\\d*(\\.\\d+)?")) {
+        		num += str;
+        	}else {
+        		if(!num.equals("")) {
+        			answer += Integer.parseInt(num);
+        			num= "";
+        		}
+        	}
+        }
+        if(!num.equals("")) answer += Integer.parseInt(num);
+        return answer;
+    }
+    
+    public int solution02(String my_string) {
+        int answer = 0;
+        String[] str = my_string.replaceAll("[a-zA-Z]", " ").split(" ");
+        for(String s : str){
+            if(!s.equals("")) answer += Integer.parseInt(s);
+        }
+        return answer;
+    }
 }
