fcb3ceafbd1969c2b986766224238b6ab6101f17
devkgn88 <devkgn88@gmail.com>
2024-08-30 11:09:29 +0900

    [KimGanam] Divide number

---
+++ src/com/gn/day05/Solution02.java
@@ -0,0 +1,16 @@
+package com.gn.day05;
+
+public class Solution02 {
+	public int[] solution01(int money) {
+		int[] answer = new int[2];
+		answer[0] = money / 5500;
+		answer[1] = money % 5500;
+		return answer;
+	}
+	
+	public int[] solution02(int money) {
+		return new int[]{money / 5500,money % 5500};
+	}
+
+
+}
