929d604b9aa1ff525c5e71465fed5b979f960ddf
devkgn88 <devkgn88@gmail.com>
2024-09-15 23:09:18 +0900

    [KimGanam] Push String

--- src/com/gn/day25/Run.java
+++ src/com/gn/day25/Run.java
@@ -2,14 +2,21 @@ package com.gn.day25;
 
 public class Run {
 	public static void main(String[] args) {
-		Solution03 s3 = new Solution03();
-		System.out.println("=== 1 ===");
-		s3.solution(3, 12);
-		System.out.println("=== 2 ===");
-		s3.solution(5, 15);
-		System.out.println("=== 3 ===");
-		s3.solution(4, 14);
-		System.out.println("=== 4 ===");
-		s3.solution(5, 5);
+		Solution01 s1 = new Solution01();
+		s1.solution01("hello", "ohell");
+		s1.solution01("apple", "elppa");
+		s1.solution01("atat", "tata");
+		s1.solution01("abc", "abc");
+		
+		
+//		Solution03 s3 = new Solution03();
+//		System.out.println("=== 1 ===");
+//		s3.solution(3, 12);
+//		System.out.println("=== 2 ===");
+//		s3.solution(5, 15);
+//		System.out.println("=== 3 ===");
+//		s3.solution(4, 14);
+//		System.out.println("=== 4 ===");
+//		s3.solution(5, 5);
 	}
 }
---
+++ src/com/gn/day25/Solution01.java
@@ -0,0 +1,52 @@
+package com.gn.day25;
+
+public class Solution01 {
+    // In the string "hello", if we move each character
+	// one space to the right and move the last character to the front, 
+	// we get "ohell".
+	// If we define this as pushing a string, 
+	// when strings A and B are given as parameters,
+	// Complete the solution function so that if A can be pushed to reach B,
+	// it returns the minimum number of times it must be pushed, 
+	// and if it cannot be pushed to B, it returns -1.
+	public int solution02(String A, String B) {
+		return (B+B).indexOf(A);
+	}
+	
+	public int solution01(String A, String B) {
+        int answer = -1;
+        String[] arr = A.split("");
+        int len = A.length();
+        String[] brr = new String[B.length()];
+        for(int i = 0 ; i < len ; i++) {
+        	//System.out.println("=== "+i+" ===");
+        	for(int x = 0 ; x < i ; x++) {
+        		//System.out.println("brr : "+x+"arr : "+(len-i+x));
+        		brr[x] = arr[len-i+x];
+        	}
+        	for(int y = 0 ; y < len-i ; y++) {
+        		brr[i+y] = arr[y];
+        		//System.out.println((i+y)+" : "+ y);
+        	}
+        	// System.out.println(String.join("", brr));
+        	if(B.equals(String.join("", brr))) {
+        		answer = i;
+        		break;
+        	}
+        }
+        
+        // 0 1 2 3 4 -> 0 
+        // 4 0 1 2 3 -> 1
+        // 3 4 0 1 2 -> 2
+        // 2 3 4 0 1 -> 3
+        // 1 2 3 4 0 -> 4
+        
+        // 0 1 2 3
+        // 3 0 1 2
+        // 2 3 0 1
+        // 1 2 3 0
+        
+        System.out.println(answer);
+        return answer;
+    }
+}
