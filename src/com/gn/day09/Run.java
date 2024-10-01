0a91e87a0facefa8bcc7fb0aa4633835e7273eac
devkgn88 <devkgn88@gmail.com>
2024-10-01 22:24:03 +0900

    [KimGanam] morse code

--- src/com/gn/day09/Run.java
+++ src/com/gn/day09/Run.java
@@ -3,13 +3,17 @@ package com.gn.day09;
 public class Run {
 
 	public static void main(String[] args) {
-		Solution01 s1 = new Solution01();
-		System.out.println("=== hp : 23 ===");
-		s1.solution01(23);
-		System.out.println("=== hp : 24 ===");
-		s1.solution01(24);
-		System.out.println("=== hp : 999 ===");
-		s1.solution01(999);
+//		Solution01 s1 = new Solution01();
+//		System.out.println("=== hp : 23 ===");
+//		s1.solution01(23);
+//		System.out.println("=== hp : 24 ===");
+//		s1.solution01(24);
+//		System.out.println("=== hp : 999 ===");
+//		s1.solution01(999);
+		
+		Solution02 s2 = new Solution02();
+		s2.solution01(".... . .-.. .-.. ---");
+		s2.solution01(".--. -.-- - .... --- -.");
 	}
 
 }
---
+++ src/com/gn/day09/Solution02.java
@@ -0,0 +1,63 @@
+package com.gn.day09;
+
+import java.util.*;
+
+// morse code
+
+public class Solution02 {
+	
+	public String solution02(String letter) {
+        StringBuilder sb = new StringBuilder();
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        String[] word = letter.split(" ");   
+        for(String alphabet : word) {
+        	for(int i = 0 ; i < morse.length ; i++) {
+        		if(alphabet.equals(morse[i])) {
+        			sb.append((char)(i+97));
+        		}
+        	}
+        }    
+        return sb.toString();
+	}
+	
+	
+    public String solution01(String letter) {
+        String answer = "";
+        
+        Map<String,String> map = new HashMap<String,String>();
+        map.put(".-","a");
+        map.put("-...","b");
+        map.put("-.-.","c");
+        map.put("-..","d");
+        map.put(".","e");
+        map.put("..-.","f");
+        map.put("--.","g");
+        map.put("....","h");
+        map.put("..","i");
+        map.put(".---","j");
+        map.put("-.-","k");
+        map.put(".-..","l");
+        map.put("--","m");
+        map.put("-.","n");
+        map.put("---","o");
+        map.put(".--.","p");
+        map.put("--.-","q");
+        map.put(".-.","r");
+        map.put("...","s");
+        map.put("-","t");
+        map.put("..-","u");
+        map.put("...-","v");
+        map.put(".--","w");
+        map.put("-..-","x");
+        map.put("-.--","y");
+        map.put("--..","z");
+        
+        String[] arr = letter.split(" ");
+        for(String s : arr){
+            if(map.containsKey(s)){
+                answer += map.get(s);
+            }
+        }
+        return answer;
+    }
+}
